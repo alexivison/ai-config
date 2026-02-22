5155a4300fa22c958ae8a99c2aac42be57c516d4
Aleksi Tuominen <aleksi.j.tuominen@hotmail.com>
2026-02-22 22:20:25 +0900

    Remove testbed folder (moved to dedicated repo)
    Testbed now lives at alexivison/workflow-testbed.

--- testbed/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { Task, CreateTaskInput, UpdateTaskInput, Priority, Status } from './types';
-export { createTask, getTask, getAllTasks, getTasksByStatus, updateTask, deleteTask, clearAll } from './store';
-export { validateCreateInput, validateUpdateInput } from './validators';
-export { formatTaskSummary, formatTaskDetail, formatTaskList, formatDate } from './formatters';
